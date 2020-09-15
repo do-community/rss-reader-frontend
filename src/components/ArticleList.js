c62287110b22ec446d3dfd20dfe7a64488813a2e
Chris on Code <hey@chrisoncode.io>
2020-09-15 11:58:42 -0700

    added links to articles

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Timestamp from "react-timestamp";
 import { useFeeds } from "../contexts/FeedContext";
+import link from "../link.svg";
 
 export default function ArticleList() {
   const { articles } = useFeeds();
@@ -27,18 +28,38 @@ function Article({ article }) {
           isOpen ? "bg-gray-200" : ""
         }`}
       >
-        <span className="hidden lg:block w-1/6 text-gray-500 mr-6">
+        <a className="hidden lg:block w-1/6 text-gray-500 mr-6">
           {article.feed_name}
-        </span>
+        </a>
         <span className="block font-bold flex-grow">{article.title}</span>
 
         <Timestamp date={article.date} className="text-gray-500 ml-auto" />
       </button>
 
       {isOpen && (
-        <div className="p-10 prose">
-          <h2>{article.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: article.summary }} />
+        <div className="p-10">
+          <div className="mb-12 flex justify-between items-center">
+            <h2 className="text-5xl font-bold text-gray-900 hover:underline hover:text-blue-800 transition duration-200">
+              <a href={article.link} alt={article.title}>
+                {article.title}
+              </a>
+            </h2>
+
+            <div>
+              <a
+                href={article.link}
+                alt={article.title}
+                className="opacity-50 hover:opacity:75 transition duration-200"
+              >
+                <img src={link} alt="View Article" className="w-6 h-6" />
+              </a>
+            </div>
+          </div>
+
+          <div
+            className="prose"
+            dangerouslySetInnerHTML={{ __html: article.summary }}
+          />
         </div>
       )}
     </div>
